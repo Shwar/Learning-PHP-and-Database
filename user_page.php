ba32b2e01bd0a01f346aa99ee6772c7e62b523cb
Simon Kiragu <simonkiragu04@gmail.com>
2024-03-23 15:17:11 +0300

    Added files

---
+++ user_page.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="style.css">
+    <title>User Page</title>
+</head>
+<body>
+
+<div class="container">
+    
+    <div class="content">
+        <h3>Hi, <span>User</span></h3>
+        <h1>Welcome  <span></span></h1>
+        <p>This is a User page</p>
+        <a href="login_form.php" class="btn">Login</a>
+        <a href="register_form.php" class="btn">Register</a>
+        <a href="logout_form.php" class="btn">Log Out</a>
+
+
+    </div>
+</div>    
+</body>
+</html>
